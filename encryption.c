40f613ad0a53e8ed9369fb8e42b9ecb1bb8e1191
Ubuntu <ubuntu@ip-172-31-16-129.ec2.internal>
2019-11-24 00:14:19 +0000

    v1.1

--- encryption.c
+++ encryption.c
@@ -7,16 +7,24 @@
 int Fibonacci[30]; //Array with first 30 fibonacci numbers
 int Arr[15];	   //Storing Indexes of Fibonacci numbers
 
-int xortodecimal(int D1[])
+int ASCIIKeyword;
+char keyword[1];
+char Finall[10];
+
+struct originalString
 {
-	int i;
-	float decimal = 0.0;
-	for (i = 0; i < 8; i++)
-	{
-		decimal = decimal + D1[i] * pow(2.0, i);
-	}
-	return decimal;
-}
+	int length;
+	char String[100];
+	int Ascii[100];
+	unsigned long int Binary[100];
+	int XORDecimal[100];
+	unsigned long int FibonacciBinary[100];
+}S;
+
+struct Binary
+{
+	double Binary[100];
+}B;
 
 //Function4  Decimal to Fibonacci Format (Step 5)
 
@@ -28,7 +36,6 @@ void fibonacci()
 	for (i = 2; i < 30; i++)
 	{
 		Fibonacci[i] = Fibonacci[i - 1] + Fibonacci[i - 2];
-		// printf("\n%d",Fibonacci[i]);
 	}
 }
 int check(int number)
@@ -41,222 +48,133 @@ int check(int number)
 	return i;
 }
 
-int *decimaltofibonacci(int no)
+void decimaltofibonacci()
 {
-
-	static int final[8];
+	int no; //XORDecimal
+	char *eptr;
+	int final[8];
 	fibonacci();
-	int i, n, number, a, b, c;
-	number = no;
-	int sum = 0;
-	i = 0;
-	int z = 0;
-	while (sum != no)
+	int i, n, number, a, b, c, sum = 0, z = 0, j, k;
+	printf("\nEncrypted Text is ");
+	for(j=0; j< S.length-1; j++)
 	{
-		a = check(number) - 1;
-		if (a == 0)
+		no = S.XORDecimal[j];
+		number = no;
+		i = 0;
+		z = 0;
+		sum = 0;
+		while (sum != no)
 		{
-			Arr[i] = 1;
-			sum++;
-			i++;
-			z++;
-		}
-		else if (a < 0)
-		{
-			break;
-		}
-		else
-		{
-			number = number - Fibonacci[a];
-			sum += Fibonacci[a];
-			Arr[i] = a;
-			z++;
-			i++;
-		}
-	}
-	/*
-	printf("Hello : ");
-	for(i=0;i<8;i++)
-	{
-		printf("%d ",Arr[i]);
-	}
-	*/
-
-	printf("\n");
-
-	/*
-Hey there
-
-	int size = sizeof(Arr)/sizeof(Arr[0]); 
-	//printf("\nSize : %d\n", z);
-	for(i=0;i<8;i++)
-	{
-		int j;
-		int con=0;
-		for(j=0;j<z;j++)
-		{
-			if(i==(Arr[j]-1))
+			a = check(number) - 1;
+			if (a == 0)
 			{
-				con++;
-				//break;
+				Arr[i] = 1;
+				sum++;
+				i++;
+				z++;
+			}
+			else if (a < 0)
+			{
+				break;
 			}
 			else
 			{
-			}		
+				number = number - Fibonacci[a];
+				sum += Fibonacci[a];
+				Arr[i] = a;
+				z++;
+				i++;
+			}
 		}
-		if(con==1)
-		final[i]=1;
-		else
-		final[i]=0;
-	}
+		printf("\n");
 
-Bye there
-*/
-
-	for (i = 0; i < 8; i++)
-	{
-		final[i] = 0;
+		for (i = 0; i < 8; i++)
+		{
+			final[i] = 0;
+		}
+		int q = 0;
+		while (q < z)
+		{
+			final[Arr[q++] - 1] = 1;
+		}
+		FILE *fp;
+		fp=fopen("abc.txt","w");     
+		for(k=0;k<8;k++) 
+		{
+			fprintf(fp,"%d",final[k]);
+		}	
+		fclose(fp);
+		fp = fopen("abc.txt","r");
+		//fscanf(fp, "%lu", &Finally);
+		//printf("%lu", Finally);
+		fscanf(fp ,"%s", Finall);
+		fclose(fp);
+		//printf("\n%s", Finall);
+		S.FibonacciBinary[j] = strtol(Finall, &eptr, 2);
+		printf("%lu ", S.FibonacciBinary[j]);
 	}
-	int q = 0;
-	while (q < z)
-	{
-		final[Arr[q++] - 1] = 1;
-	}
-
-	return final;
+	printf("\n");
+	//S.XORDecimal[j] = final;
 }
-
 //Function3 Converting XORed output to decimal (Step 4)
 
 //Function2 XOR the plain letter and keyword letter (Step 3)
-
-int *binaryXOR(int A1[], int B1[])
+void binaryXOR()
 {
 	int i;
-	static int C1[8];
-	for (i = 0; i < 8; i++)
+	for(i=0;i<S.length-1;i++)
 	{
-		if (A1[i] == B1[i])
-		{
-			C1[i] = 0;
-			;
-		}
-		else
-		{
-			C1[i] = 1;
-		}
+		S.XORDecimal[i] = S.Ascii[i] ^ ASCIIKeyword;
+		//printf("\n%d",S.XORDecimal[i]);
 	}
-
-	//printf("\n");
-	return C1;
 }
 
 //Function1 Converting ascii code to binary code (Preliminary Step)
 
-int *decimaltobinary(int n)
-{
-	static int a[8];
-	int i = 0, c = 0;
-	while (n > 0)
+void decToBinary() 
+{ 
+	int i;
+	int j,k;
+	int dn,n;
+	int binno;
+	for(k=0; k<S.length-1; k++)
 	{
-		if (n % 2 == 0)
+		i = 1;
+		binno = 0;
+		n = S.Ascii[k];
+		dn = n;
+		for(j=n;j>0;j=j/2)
 		{
-			a[i] = 0;
-			i++;
+			binno=binno+(n%2)*i;
+			i=i*10;
+			n=n/2;
 		}
-		else
-		{
-			a[i] = 1;
-			i++;
-		}
-		n = n / 2;
-		c++;
+		S.Binary[k] = (long int)binno;
 	}
-	printf("\n");
-	for (i = c; i < 8; i++)
-	{
-		a[i] = 0;
-	}
-	/*for(i=7;i>=0;i--)
-		{
-			printf("%d",a[i]);		
-		}*/
-	return a;
 }
 
 int main()
 {
-	char originalstring[10];
-	char keyword[1];
 	printf("Enter Keyword letter\n");
 	fgets(keyword, MAX_LIMIT, stdin);
 	printf("Enter a string\n");
-	fgets(originalstring, MAX_LIMIT, stdin);
-	printf("%c ****",originalstring[0]);
-	int as1 = (int)originalstring[0];
-	int as2 = (int)keyword[0];
+	fgets(S.String, MAX_LIMIT, stdin);
+	for(S.length = 0; S.String[S.length] != '\0'; S.length++);
+	int j;
+	for(j = 0; j<S.length-1; j++)
+	{
+		S.Ascii[j] = (int)S.String[j];
+		//printf("\n%d",S.Ascii[j]);
+	}
+	ASCIIKeyword = (int)keyword[0];
 	printf("\n");
-	printf("ASCII code of plain letter = %d \nASCII code of keyword letter = %d\n", as1, as2);
-
-	/*
-	int *A2=decimaltobinary(as1);
-	int *B2=decimaltobinary(as2);
-
-	printf("\n%d\n%d",*A2,*B2);
-	*/
-	int i;
-	int *bin1 = decimaltobinary(as1);
-	int arr1[8];
-	printf("Binary code of plain letter   = ");
-	for (i = 7; i >= 0; i--)
-	{
-		arr1[i] = bin1[i];
-		printf("%d ", bin1[i]);
-	}
-	printf("\nBinary code of keyword letter = ");
-	int *bin2 = decimaltobinary(as2);
-	int arr2[8];
-	for (i = 7; i >= 0; i--)
-	{
-		arr2[i] = bin2[i];
-		printf("%d ", bin2[i]);
-	}
+	decToBinary();
+	// for(j = 0; j<S.length; j++)
+	// {
+	// 	printf("\n%lu",S.Binary[j]);
+	// }
 	printf("\n");
-
-	printf("XOR of plain letter and keyword letter = ");
-	int arr4[8];
-	int *xor = binaryXOR(arr1, arr2);
-	for (i = 7; i >= 0; i--)
-	{
-		arr4[i] = xor[i];
-		printf("%d ", xor[i]);
-	}
-	printf("\nDecimal of XORed code = ");
-	int deci = xortodecimal(arr4);
-	printf("%d\n", deci);
-	int *fibo = decimaltofibonacci(deci);
-	printf("\nFibonacci Format = ");
-	int arr5[8];
-	for (i = 7; i >= 0; i--)
-	{
-		arr5[i] = fibo[i];
-		printf("%d ", fibo[i]);
-	}
-	printf("\n");
-
-	// Converting the fibonacii format to decimal as final code to be sent (Step 6)
-
-	int fibodeci = xortodecimal(arr5);
-	printf("Final code to be sent = %d\n", fibodeci);
-
-	return 0;
-
-	
+	binaryXOR();
+	decimaltofibonacci();
+	return 0;	
 }
-
-//--------------------------------------------------------------------
-
-/*
-	char *args[]={"./decryption", NULL};
-	execvp(args[0],args);
-*/
